1972d446c4864724db81f1e365759f88fc914f93
DESKTOP-C87FJFI\赤枝佳樹 <y.a.05081996@gmail.com>
2018-05-18 16:37:10 +0900

    表を改善しようとしている（未完成）

--- read_data.js
+++ read_data.js
@@ -18,17 +18,19 @@ $(document).ready(function () {
         if (data == null) alert('データが0件でした');
         // read_data = data.concat();
         // read_data = data;
-        create_table(data);
+
         // set_read_data(data);
         // 返ってきたデータの表示
-        var $content = $('#content');
-        for (var i = 0; i < data.length; i++) {
-            $content.append(data[i].id + "|" + data[i].name + "|" + data[i].grade + "&nbsp;&nbsp;");
-            data_id[i] = data[i].id;// data_id.push(data[i].id);
-            data_name.push(data[i].name);
-            data_grade.push(data[i].grade);
-            $content.append(data_id[i] + "|" + data_name[i] + "|" + data_grade[i] + "<br>");
-        }
+        // var $content = $('#content');
+        // $content.append(data);
+        // for (var i = 0; i < data.length; i++) {
+        //     $content.append(data[i].id + "|" + data[i].name + "|" + data[i].grade + "&nbsp;&nbsp;");
+        //     data_id[i] = data[i].id;// data_id.push(data[i].id);
+        //     data_name.push(data[i].name);
+        //     data_grade.push(data[i].grade);
+        //     $content.append(data_id[i] + "|" + data_name[i] + "|" + data_grade[i] + "<br>");
+        // }
+        create_table(data);
     }).fail(function (XMLHttpRequest, textStatus, errorThrown) {
         // 通常はここでtextStatusやerrorThrownの値を見て処理を切り分けるか、単純に通信に失敗した際の処理を記述します。
 
@@ -39,28 +41,33 @@ $(document).ready(function () {
         alert('Error : ' + errorThrown);
     });
 
-    for (var i = 0; i < 10; i++) {
-        // $content.append(data[i].id + "||" + data[i].name + "|" + data[i].grade + "&nbsp;&nbsp;");
-        // data_id[i] = data[i].id;// data_id.push(data[i].id);
-        // data_name.push(data[i].name);
-        // data_grade.push(data[i].grade);
-        // $content.append(data_id[i] + "||" + data_name[i] + "|" + data_grade[i] + "<br>");
-    }
+    // for (var i = 0; i < 10; i++) {
+    // $content.append(data[i].id + "||" + data[i].name + "|" + data[i].grade + "&nbsp;&nbsp;");
+    // data_id[i] = data[i].id;// data_id.push(data[i].id);
+    // data_name.push(data[i].name);
+    // data_grade.push(data[i].grade);
+    // $content.append(data_id[i] + "||" + data_name[i] + "|" + data_grade[i] + "<br>");
+    // }
 });
 
 function create_table(read_data) {
-
-    // for (var i = 0; i < 3; i++) {
+    // var read_data = data;
+    // document.write("<br><br><br><br><br><br><br>sdasdaas");
+    // document.write("<table id='mytable'><tr><th>氏名</th><th>ID</th><th>学年</th></tr>");
+    // for (var i = 0; i < read_data.length; i++) {
     //     var url = "http://192.168.0.159/2018grade4/kaihatu_zemi/akaeda/EESystem_S/delete.php";
-    //     document.write("<tr><td><a href='input.html?input_name=" + data_name[i] + "&input_id=" + data_id[i] + "&input_grade=" + data_grade[i] + "' class='square_btn'>" + data_name[i] + "</a>");
+    //     document.write("<tr><td><a href='input.html?input_name=" + read_data[i].name + "&input_id=" + read_data[i].id + "&input_grade=" + read_data[i].grade + "' class='square_btn'>" + read_data[i].name + "</a>");
     //     document.write("&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;");
-    //     document.write("<a href='" + url + "?input_id=" + data_id[i] + "'class='square_btn'>削除</a></td>");
+    //     document.write("<a href='" + url + "?input_id=" + read_data[i].id + "'class='square_btn'>削除</a></td>");
     //     // document.write(" <tr><td>", data_id[i], "</td>");
-    //     document.write("<td>" + data_id[i] + "</td>");
-    //     document.write("<td>" + data_gradea + "</td></tr>");
+    //     document.write("<td>" + read_data[i].id + "</td>");
+    //     document.write("<td>" + i + "</td></tr>");
 
     // }
+    // document.write("</table>");
+
     var $content = $('#read');
+    $content.append("<table id='mytable'><tr><th>氏名</th><th>ID</th><th>学年</th></tr>");
     for (var i = 0; i < read_data.length; i++) {
         var url = "http://192.168.0.159/2018grade4/kaihatu_zemi/akaeda/EESystem_S/delete.php";
         $content.append("<tr><td><a href='input.html?input_name=" + read_data[i].name + "&input_id=" + read_data[i].id + "&input_grade=" + read_data[i].grade + "' class='square_btn'>" + read_data[i].name + "</a>");
@@ -71,6 +78,6 @@ function create_table(read_data) {
         $content.append("<td>" + read_data[i].grade + "</td></tr>");
 
     }
-    // $content.append($HTML).trigger('create');
+    $content.append("</table>");
 }
 
