2a6873753db232f4615a7e147d24f8a87dd1168b
akaeda-yoshiki <y.a.05081996@gmail.com>
2018-05-28 02:56:24 +0900

    項目の修正、新規ファイルの追加

--- header.js
+++ header.js
@@ -14,21 +14,31 @@ function write_header(faze) {
         case 0://登録
             html += '<a href="input.html" class="tab_b">登録</a>';
             html += '<a href="comfirm.html" class="tab_g">登録者</a>';
+            html += '<a href="real_time.html" class="tab_g">リアルタイム</a>';
             html += '<a href="search.html" class="tab_g">入退室データ</a>';
             break;
         case 1://確認
             html += '<a href="input.html" class="tab_g">登録</a>';
             html += '<a href="comfirm.html" class="tab_b">登録者</a>';
+            html += '<a href="real_time.html" class="tab_g">リアルタイム</a>';
             html += '<a href="search.html" class="tab_g">入退室データ</a>';
             break;
-        case 2://検索
+        case 2://リアルタイム
             html += '<a href="input.html" class="tab_g">登録</a>';
             html += '<a href="comfirm.html" class="tab_g">登録者</a>';
+            html += '<a href="real_time.html" class="tab_b">リアルタイム</a>';
+            html += '<a href="search.html" class="tab_g">入退室データ</a>';
+            break;
+        case 3://検索
+            html += '<a href="input.html" class="tab_g">登録</a>';
+            html += '<a href="comfirm.html" class="tab_g">登録者</a>';
+            html += '<a href="real_time.html" class="tab_g">リアルタイム</a>';
             html += '<a href="search.html" class="tab_b">入退室データ</a>';
             break;
         default:
             html += '<a href="input.html" class="tab_g">登録</a>';
             html += '<a href="comfirm.html" class="tab_g">登録者</a>';
+            html += '<a href="real_time.html" class="tab_g">リアルタイム</a>';
             html += '<a href="search.html" class="tab_g">入退室データ</a>';
             break;
     }
