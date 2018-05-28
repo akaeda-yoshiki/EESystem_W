fa3e20249f78e5a67aaafd9243b691985132bfa2
DESKTOP-C87FJFI\赤枝佳樹 <y.a.05081996@gmail.com>
2018-05-28 22:50:44 +0900

    動的に生成したボタンを埋め込んだ表をきれいに表示

--- read_data.js
+++ read_data.js
@@ -66,59 +66,58 @@ function create_table(read_data) {
     // }
     // document.write("</table>");
 
-    // var $content = $('#read');
-    // $content.append("<table id='mytable'><tr><th>氏名</th><th>ID</th><th>学年</th></tr>");
-    // for (var i = 0; i < read_data.length; i++) {
-    //     var url = "http://192.168.0.159/2018grade4/kaihatu_zemi/akaeda/EESystem_S/delete.php";
-    //     $content.append("<tr><td><a href='input.html?input_name=" + read_data[i].name + "&input_id=" + read_data[i].id + "&input_grade=" + read_data[i].grade + "' class='square_btn'>" + read_data[i].name + "</a>");
-    //     $content.append("&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;");
-    //     $content.append("<a href='" + url + "?input_id=" + read_data[i].id + "'class='square_btn'>削除</a></td>");
-    //     // document.write(" <tr><td>", data_id[i], "</td>");
-    //     $content.append("<td>" + read_data[i].id + "</td>");
-    //     $content.append("<td>" + read_data[i].grade + "</td></tr>");
+    var $content = $('#read');
+    $content.append("<table id='mytable'><tr><th>氏名</th><th>ID</th><th>学年</th></tr>");
+    var url = "http://192.168.0.159/2018grade4/kaihatu_zemi/akaeda/EESystem_S/delete.php";
+    for (var i = 0; i < read_data.length; i++) {
+        $content.append("<tr><td><a href='input.html?input_name=" + read_data[i].name + "&input_id=" + read_data[i].id + "&input_grade=" + read_data[i].grade + "' class='square_btn'>" + read_data[i].name + "</a>");
+        $content.append("&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;");
+        $content.append("<a href='" + url + "?input_id=" + read_data[i].id + "'class='square_btn'>削除</a></td>");
+        $content.append("<td>" + read_data[i].id + "</td>");
+        $content.append("<td>" + read_data[i].grade + "</td></tr>");
 
-    // }
-    // $content.append("</table>");
+    }
+    $content.append("</table>");
     // var ee = $content.get(0);
     // ee.innerHTML = ee.innerHTML;
-    var tbody = document.createElement('tbody')
-    var th = document.createElement('th')
-    th.appendChild(document.createTextNode("氏名"))
-    var th1 = document.createElement('th')
-    th1.appendChild(document.createTextNode("ID"))
-    var th2 = document.createElement('th')
-    th2.appendChild(document.createTextNode("学年"))
-    var tr = document.createElement('tr')
-    tr.appendChild(th)
-    tr.appendChild(th1)
-    tr.appendChild(th2)
-    tbody.appendChild(tr)
-    for (var i = 0; i < read_data.length; i++) {
+    // var tbody = document.createElement('tbody')
+    // var th = document.createElement('th')
+    // th.appendChild(document.createTextNode("氏名"))
+    // var th1 = document.createElement('th')
+    // th1.appendChild(document.createTextNode("ID"))
+    // var th2 = document.createElement('th')
+    // th2.appendChild(document.createTextNode("学年"))
+    // var tr = document.createElement('tr')
+    // tr.appendChild(th)
+    // tr.appendChild(th1)
+    // tr.appendChild(th2)
+    // tbody.appendChild(tr)
+    // for (var i = 0; i < read_data.length; i++) {
    
-	var td = document.createElement('td')
-    td.appendChild(document.createTextNode(read_data[i].name))
-    // td.innerHTML = "<a href='" + url + "?input_id=" + read_data[i].id + "'class='square_btn'>削除</a>"
-    var td1 = document.createElement('td')
-    td1.appendChild(document.createTextNode(read_data[i].id))
-    var td2 = document.createElement('td')
-    td2.appendChild(document.createTextNode(read_data[i].grade))
+	// var td = document.createElement('td')
+    // td.appendChild(document.createTextNode(read_data[i].name))
+    // // td.innerHTML = "<a href='" + url + "?input_id=" + read_data[i].id + "'class='square_btn'>削除</a>"
+    // var td1 = document.createElement('td')
+    // td1.appendChild(document.createTextNode(read_data[i].id))
+    // var td2 = document.createElement('td')
+    // td2.appendChild(document.createTextNode(read_data[i].grade))
     
-    // var but = document.createElement('BUTTON')
-    // $sampleButton.textContent = "サンプル";
-    // td.appendChild(but)
-	var tr = document.createElement('tr')
-	// tr.appendChild(th)
-    tr.appendChild(td)
-    tr.appendChild(td1)
-    tr.appendChild(td2)
+    // // var but = document.createElement('BUTTON')
+    // // $sampleButton.textContent = "サンプル";
+    // // td.appendChild(but)
+	// var tr = document.createElement('tr')
+	// // tr.appendChild(th)
+    // tr.appendChild(td)
+    // tr.appendChild(td1)
+    // tr.appendChild(td2)
 
-    //tbodyが必要
+    // //tbodyが必要
 	
-    tbody.appendChild(tr)
-    }
-	var table = document.createElement('table')
-	table.appendChild(tbody)
-	//bodyに追加
-	document.getElementsByTagName('body')[0].appendChild(table)
+    // tbody.appendChild(tr)
+    // }
+	// var table = document.createElement('table')
+	// table.appendChild(tbody)
+	// //bodyに追加
+	// document.getElementsByTagName('body')[0].appendChild(table)
 }
 
