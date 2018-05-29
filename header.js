ea1d6a5b75f14101efef5f8cc85f69a198abb955
DESKTOP-C87FJFI\赤枝佳樹 <y.a.05081996@gmail.com>
2018-05-29 15:21:01 +0900

    項目名の変数化、ログイン時にIDとパスワードをサーバー側で確認

--- header.js
+++ header.js
@@ -1,3 +1,5 @@
+var word = Array("新規登録", "登録者", "リアルタイム", "入退室データ");
+
 // 各画面で共通部分
 function write_header(faze) {
     var html = "";
@@ -13,34 +15,34 @@ function write_header(faze) {
     html += '<br>';
     switch (faze) {//引数によって選んでいる項目の淵を太枠で囲い、文字を青くする
         case 0://登録
-            html += '<a href="input.html" class="tab_b">登録</a>';
-            html += '<a href="user.html" class="tab_g">登録者</a>';
-            html += '<a href="real_time.html" class="tab_g">リアルタイム</a>';
-            html += '<a href="data.html" class="tab_g">入退室データ</a>';
+            html += '<a href="input.html" class="tab_b">' + word[0] + '</a>';
+            html += '<a href="user.html" class="tab_g">' + word[1] + '</a>';
+            html += '<a href="real_time.html" class="tab_g">' + word[2] + '</a>';
+            html += '<a href="data.html" class="tab_g">' + word[3] + '</a>';
             break;
         case 1://確認
-            html += '<a href="input.html" class="tab_g">登録</a>';
-            html += '<a href="user.html" class="tab_b">登録者</a>';
-            html += '<a href="real_time.html" class="tab_g">リアルタイム</a>';
-            html += '<a href="data.html" class="tab_g">入退室データ</a>';
+            html += '<a href="input.html" class="tab_g">' + word[0] + '</a>';
+            html += '<a href="user.html" class="tab_b">' + word[1] + '</a>';
+            html += '<a href="real_time.html" class="tab_g">' + word[2] + '</a>';
+            html += '<a href="data.html" class="tab_g">' + word[3] + '</a>';
             break;
         case 2://リアルタイム
-            html += '<a href="input.html" class="tab_g">登録</a>';
-            html += '<a href="user.html" class="tab_g">登録者</a>';
-            html += '<a href="real_time.html" class="tab_b">リアルタイム</a>';
-            html += '<a href="data.html" class="tab_g">入退室データ</a>';
+            html += '<a href="input.html" class="tab_g">' + word[0] + '</a>';
+            html += '<a href="user.html" class="tab_g">' + word[1] + '</a>';
+            html += '<a href="real_time.html" class="tab_b">' + word[2] + '</a>';
+            html += '<a href="data.html" class="tab_g">' + word[3] + '</a>';
             break;
         case 3://検索
-            html += '<a href="input.html" class="tab_g">登録</a>';
-            html += '<a href="user.html" class="tab_g">登録者</a>';
-            html += '<a href="real_time.html" class="tab_g">リアルタイム</a>';
-            html += '<a href="data.html" class="tab_b">入退室データ</a>';
+            html += '<a href="input.html" class="tab_g">' + word[0] + '</a>';
+            html += '<a href="user.html" class="tab_g">' + word[1] + '</a>';
+            html += '<a href="real_time.html" class="tab_g">' + word[2] + '</a>';
+            html += '<a href="data.html" class="tab_b">' + word[3] + '</a>';
             break;
         default:
-            html += '<a href="input.html" class="tab_g">登録</a>';
-            html += '<a href="user.html" class="tab_g">登録者</a>';
-            html += '<a href="real_time.html" class="tab_g">リアルタイム</a>';
-            html += '<a href="data.html" class="tab_g">入退室データ</a>';
+            html += '<a href="input.html" class="tab_b">' + word[0] + '</a>';
+            html += '<a href="user.html" class="tab_g">' + word[1] + '</a>';
+            html += '<a href="real_time.html" class="tab_g">' + word[2] + '</a>';
+            html += '<a href="data.html" class="tab_g">' + word[3] + '</a>';
             break;
     }
 
